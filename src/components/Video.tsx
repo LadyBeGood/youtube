3321b2cfb0f1afe037a76a81fc63cc49e845c28e
LadyBeGood <infinixutkarsh@gmail.com>
2025-08-18 13:59:03 +0530

    Optimised

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,7 +3,7 @@ const Video = ({ isMusical = false }: { isMusical?: boolean }) => {
     return (
         <button>
             <div className="relative">
-                <img src="./thumbnail-2.jpg" alt="" />
+                <img src="./thumbnail.webp" alt="" />
                 <div className="absolute rounded right-2 bottom-1 bg-[#00000090] flex text-[10px] justify-around text-white items-center px-1.5 py-0.5 ">
                     {isMusical &&
                         <span className="material-symbols-outlined" style={{ fontSize: "12px", fontVariationSettings: `"FILL" 1, "wght" 400,"GRAD" 0` }}>music_note</span>
@@ -12,7 +12,7 @@ const Video = ({ isMusical = false }: { isMusical?: boolean }) => {
                 </div>
             </div>
             <div className="py-2 px-1 gap-3 pb-5 flex items-start">
-                <img src="./avatar.jpg" alt="channel" className="w-12 aspect-square rounded-full mt-2" />
+                <img src="./avatar.webp" alt="channel" className="w-12 aspect-square rounded-full mt-2" />
                 <div className="gap-1 flex flex-col">
                     <div className="text-left">How I Animate Stuff On Desmos Graphing Calculator</div>
                     <div className="text-xs text-left text-cool-gray">
